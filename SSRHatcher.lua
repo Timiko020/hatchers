ab94428e6ad629c208e622e5364da831a353f0b8
Timiko020 <38922928+Timiko020@users.noreply.github.com>
2022-09-04 11:23:12 +0200

    Update SSRHatcher.lua

--- SSRHatcher.lua
+++ SSRHatcher.lua
@@ -1,22 +1,3 @@
-local function rgbToHex(rgb)
-    local hexadecimal = '0X'
-    for key, value in pairs(rgb) do
-       local hex = ''
-       while(value > 0)do
-    local index = math.fmod(value, 16) + 1
-    value = math.floor(value / 16)
-    hex = string.sub('0123456789ABCDEF', index, index) .. hex
-    end
-    if(string.len(hex) == 0)then
-    hex = '00'
-    elseif(string.len(hex) == 1)then
-    hex = '0' .. hex
-    end
-    hexadecimal = hexadecimal .. hex
-    end
-    return hexadecimal
-    end
-    
     local username = game:GetService("Players").LocalPlayer.Name
     local userid = game:GetService("Players").LocalPlayer.UserId
     local icon = "https://www.roblox.com/headshot-thumbnail/image?userId="..userid.."&width=420&height=420&format=png"
